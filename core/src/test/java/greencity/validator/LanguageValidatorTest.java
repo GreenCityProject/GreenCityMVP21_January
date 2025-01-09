38f5611b94a04ffcc0084cda05dbbcb8db316ec4
Yurii <fdyura55@gmail.com>
2025-01-09 16:10:41 +0200

    [257] Create unit test to LanguageValidator (Created LanguageValidatorTest and some tests)

---
+++ core/src/test/java/greencity/validator/LanguageValidatorTest.java
@@ -0,0 +1,60 @@
+package greencity.validator;
+
+import greencity.service.LanguageService;
+import jakarta.validation.ConstraintValidatorContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class LanguageValidatorTest {
+
+    @Mock
+    private LanguageService languageService;
+
+    @Mock
+    private ConstraintValidatorContext constraintValidatorContext;
+
+    @InjectMocks
+    private LanguageValidator languageValidator;
+
+    private List<String> languageCodes;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        languageCodes = Arrays.asList("en", "ua", "fr");
+        when(languageService.findAllLanguageCodes()).thenReturn(languageCodes);
+        languageValidator.initialize(null);
+    }
+
+    @Test
+    void testIsValidWithValidLanguage(){
+        Locale locale = new Locale("en");
+        assertTrue(languageValidator.isValid(locale, constraintValidatorContext));
+    }
+
+    @Test
+    void testIsValidWithInvalidLanguage(){
+        Locale locale = new Locale("de");
+        assertFalse(languageValidator.isValid(locale, constraintValidatorContext));
+    }
+
+    @Test
+    void testIsValidWithNull(){
+        assertFalse(languageValidator.isValid(null, constraintValidatorContext));
+    }
+
+}
