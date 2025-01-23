307f1f718110bfd0e5c38cbd01a3d204441c3343
Oleksandra Bulhakova <alexbulgakovapro@gmail.com>
2025-01-23 22:45:39 +0200

    Deleted redundant imports

--- service-api/src/main/java/greencity/dto/event/ImageRequestDto.java
+++ service-api/src/main/java/greencity/dto/event/ImageRequestDto.java
@@ -1,7 +1,5 @@
 package greencity.dto.event;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
