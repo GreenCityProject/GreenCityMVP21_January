9abc872f074836b320f06c6e7829e63f6bfb2d32
Oleksandra Bulhakova <alexbulgakovapro@gmail.com>
2025-01-26 21:43:28 +0200

    Finished EventCommentRepoTest

--- dao/src/main/java/greencity/repository/EventCommentRepo.java
+++ dao/src/main/java/greencity/repository/EventCommentRepo.java
@@ -2,7 +2,6 @@ package greencity.repository;
 
 import greencity.entity.Event;
 import greencity.entity.EventComment;
-import greencity.entity.EventDateInfo;
 import greencity.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
