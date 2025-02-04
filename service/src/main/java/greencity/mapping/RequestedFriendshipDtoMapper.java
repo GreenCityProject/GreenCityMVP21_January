883c439cddd8a7d3cb28c944376859ac42bcd0e2
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-02-04 22:06:33 +0100

    Enhancing RequestedFriendshipDto::convertwith null checks

--- service/src/main/java/greencity/mapping/RequestedFriendshipDtoMapper.java
+++ service/src/main/java/greencity/mapping/RequestedFriendshipDtoMapper.java
@@ -5,11 +5,17 @@ import greencity.entity.Friendship;
 import org.modelmapper.AbstractConverter;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+import java.util.Optional;
+
 @Component
 public class RequestedFriendshipDtoMapper extends AbstractConverter<Friendship, RequestedFriendshipDto> {
 
     @Override
     protected RequestedFriendshipDto convert(Friendship source) {
+        if(Objects.isNull(source) || Objects.isNull(source.getUser()) || Objects.isNull(source.getFriend())) {
+            throw new NullPointerException("RequestedFriendshipDto::convert argument source or user or friend is null");
+        }
         return new RequestedFriendshipDto(source.getUser().getId(), source.getFriend().getId());
     }
 }
